e05cb620163f7c9dae6b191386fde509baef950b
Omnimble <omnimble@gmail.com>
2021-03-27 17:06:30 +0200

    mongo connects

--- week2/app.js
+++ week2/app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const catRoute = require("./routes/catRoute");
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute.js");
+require('dotenv').config();
+const db = require('./database/db');
 const passport = require("./utils/pass.js");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+
+db.on('connected', () => {
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    console.log('wee');
+}
+);
 
 app.use(cors());
 
@@ -18,5 +26,3 @@ app.use(express.static("week2_public_html"));
 app.use("/cat", passport.authenticate("jwt", { session: false }), catRoute);
 app.use("/user", passport.authenticate("jwt", { session: false }), userRoute);
 app.use("/auth", authRoute);
-
-app.listen(port, () => console.log("Example app listening on port ${port}!"));
--- week2/index.js
+++ week2/index.js
@@ -4,10 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./db');
+const port = 3000;
 
 app.use(express.urlencoded({extended: false}));
-app.use('/cat', require('./routes'));
+app.use('/cat', require('./route'));
 
 db.on('connected', () => {
-  app.listen(3000);
-});
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    console.log('wee');
+}
+);
